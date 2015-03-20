21eb7f65d20f83f25a09dd51804735008bd85af3
Eric Oligney <EOengineer@gmail.com>
2015-03-20 00:08:36 -0400

    initial tests for movies index with setup and teardown.

--- test/features/can_access_movie_test.rb
+++ test/features/can_access_movie_test.rb
@@ -1,8 +1,29 @@
 require "test_helper"
 
 feature "CanAccessMovie" do
-  scenario "the test is sound" do
+
+  setup { build_movies }
+  teardown { Movie.delete_all}
+
+  scenario "it welcomes" do
     visit movies_path
     page.must_have_content "Welcome to YuReview!"
+  end
+
+  scenario 'displays movies index' do
+    visit movies_path
+    page.must_have_content "#{Movie.first.title}"
+    page.must_have_content "#{Movie.second.title}"
+    page.must_have_content "#{Movie.third.title}"
+    page.must_have_content "#{Movie.fourth.title}"
+    page.must_have_content "#{Movie.fifth.title}"
+  end
+
+  private
+
+  def build_movies
+    5.times do |n|
+      Movie.create(title: "title #{n}", description: "description #{n}", year: 1990)
+    end
   end
 end
