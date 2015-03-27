4218538fd2b0d17dda3ef291b2e17ecaf18f0671
Eric Oligney <EOengineer@gmail.com>
2015-03-27 13:17:08 -0400

    added unit tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe User do
-  # let(:user) { User.new }
 
-  # it "must be valid" do
-  #   user.must_be :valid?
-  # end
+  context 'required fields' do
+    should validate_presence_of(:email)
+    should validate_presence_of(:first_name)
+    should validate_presence_of(:password)
+  end
+
+  context 'required with data' do
+    should allow_value('test@test.com').for(:email)
+    should_not allow_value(nil).for(:email)
+    should_not allow_value('').for(:email)
+
+    should allow_value('name').for(:first_name)
+    should_not allow_value(nil).for(:first_name)
+    should_not allow_value('').for(:first_name)
+  end
+
+  context 'verifies password format' do
+    should allow_value('password').for(:password)
+    should_not allow_value(nil).for(:password)
+    should_not allow_value('').for(:password)
+    should_not allow_value('four').for(:password)
+  end
 end
