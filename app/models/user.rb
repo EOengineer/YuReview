1d5bfa1fd071185586490ff0426773009d61bbd1
Eric Oligney <EOengineer@gmail.com>
2015-03-28 09:00:39 -0400

    added double submit protection on user sign up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :password, length: { minimum: 5}
 end
