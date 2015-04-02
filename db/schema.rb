be010f148f3857a037e606f8b82fe351bd74ca74
Eric Oligney <EOengineer@gmail.com>
2015-04-01 20:52:48 -0400

    admin migration to start admin section

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329012411) do
+ActiveRecord::Schema.define(version: 20150402001003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,12 +36,13 @@ ActiveRecord::Schema.define(version: 20150329012411) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
+    t.string   "email",                           null: false
     t.string   "password_digest"
-    t.string   "first_name",      null: false
+    t.string   "first_name",                      null: false
     t.string   "last_name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
