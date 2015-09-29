ab9e543aa1504ad69ddc1d5baa1b5af0ad176d13
EOligney <Eoligney@arthrex.com>
2015-09-29 17:02:34 -0400

    removed validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,7 +7,6 @@ class Review < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
   validates :rating, presence: true, inclusion: { in: RATINGS }
-  validates :movie_id, presence: true, uniqueness: { scope: :user_id, message: "already reviewed."}
   validates :user_id, presence: true
 
 
